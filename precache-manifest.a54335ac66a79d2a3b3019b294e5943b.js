6e381bb8cd8bb9ac5c28c00ed95896ae12f812ca
simkovat <simkovat@gmail.com>
2019-06-21 22:55:55 +0200

    Updates

---
+++ precache-manifest.a54335ac66a79d2a3b3019b294e5943b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "12833d8edc2f6bd09e1e",
+    "url": "/ravenous/static/js/main.12833d8e.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/ravenous/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "12833d8edc2f6bd09e1e",
+    "url": "/ravenous/static/css/main.ce1a6964.chunk.css"
+  },
+  {
+    "revision": "96aa7b25526aa2a0f0ffe5790b3c988b",
+    "url": "/ravenous/index.html"
+  }
+];
